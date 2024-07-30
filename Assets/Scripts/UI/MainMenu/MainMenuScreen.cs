37a292475deb2bd8dd0862f1ebbf9e5c36c151af
Андрій Левицький <kn201_laa@student.ztu.edu.ua>
2024-07-30 15:19:47 +0300

    MainMenu and Title

---
+++ Assets/Scripts/UI/MainMenu/MainMenuScreen.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer _title;
+    [SerializeField] Transform _startLabel;
+    
+    private Sequence _titleSequence;
+    private Sequence _startLabelSequence;
+    
+    public void Start()
+    {
+        _titleSequence = DOTween.Sequence()
+            .Append(_title.DOFade(0, 1f).SetEase(Ease.OutFlash, 14, 1))
+            .AppendInterval(3)
+            .Append(_title.DOFade(0, 1f).SetEase(Ease.OutFlash, 10, 1))
+            .AppendInterval(4)
+            .SetLoops(-1);
+
+        _startLabelSequence = DOTween.Sequence()
+            .Append(_startLabel.DOMoveY(-0.1f, 0.5f).SetEase(Ease.OutQuad))
+            .Append(_startLabel.DOMoveY(-0.3f, 0.5f).SetEase(Ease.OutQuad))
+            .SetLoops(-1);
+    }
+
+    public void Stop()
+    {
+        _titleSequence.Kill();
+        _startLabelSequence.Kill();
+    }
+}
